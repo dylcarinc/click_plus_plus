9f37465abc14271e2405569178ee38d7e90a517a
Joseph Washum <Jkwashu@gmail.com>
2024-10-12 15:09:12 -0500

    Created scoreboard and user profile pages, updated homepage
    Created basic functionality for incrementing score and the scoreboard, users can follow another user to see them on a following tab in the scoreboard

--- lib/home.dart
+++ lib/home.dart
@@ -1,13 +1,68 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'scoreboard_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _score = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadScore();
+  }
+
+  Future<void> _loadScore() async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      final doc = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(user.uid)
+          .get();
+      if (doc.exists) {
+        setState(() {
+          _score = doc.data()?['score'] ?? 0;
+        });
+      }
+    }
+  }
+
+  Future<void> _incrementScore() async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      setState(() {
+        _score++;
+      });
+      await FirebaseFirestore.instance.collection('users').doc(user.uid).set({
+        'score': _score,
+      }, SetOptions(merge: true));
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return AppBar(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
         actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const ScoreboardScreen()),
+              );
+            },
+            child: Text('$_score', style: const TextStyle(fontSize: 20)),
+          ),
           IconButton(
             icon: const Icon(Icons.person),
             onPressed: () {
@@ -29,6 +84,13 @@ class HomeScreen extends StatelessWidget {
             },
           )
         ],
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: _incrementScore,
+          child: const Text('Increment Score'),
+        ),
+      ),
     );
   }
 }
