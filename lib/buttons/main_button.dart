a7abf2779a5df362f6286cca6fc5c3154e72cece
Mason Murphy <murphyma@hendrix.edu>
2024-10-11 22:47:55 -0500

    Created initial main_button code

--- lib/buttons/main_button.dart
+++ lib/buttons/main_button.dart
@@ -3,16 +3,31 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class MainButton extends StatefulWidget {
+  final Function(int) onCountChanged;
+
+  const MainButton({Key? key, required this.onCountChanged}) : super(key: key);
+
   @override
   _MainButtonState createState() => _MainButtonState();
 }
 
 class _MainButtonState extends State<MainButton> {
-  int _userCount = 0;
-  final String userId = FirebaseAuth.instance.currentUser!.uid;
+  int _count = 0;
 
-  
+  void _incrementCount() {
+    setState(() {
+      _count++;
+      widget.onCountChanged(_count);
+    });
+  }
 
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: _incrementCount,
+      child: const Text('Increment Count'),
+    );
+  }
 }
 
 
