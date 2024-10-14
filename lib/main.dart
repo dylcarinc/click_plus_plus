50660e843bd0233af5e4283a20230255154b16fa
Joseph Washum <Jkwashu@gmail.com>
2024-10-14 18:23:07 -0500

    added router class functionality to main, changed file structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'package:click_plus_plus/app properties/app.dart';
+import 'package:click_plus_plus/routing/app_router.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -11,4 +11,20 @@ void main() async {
   );
 
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      onGenerateRoute: AppRouter.generateRoute,
+      initialRoute: AppRouter.authGate,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+    );
+  }
 }
